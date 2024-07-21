dad3a1bf80514e8e5e126623dfe3ff270340aa08
SergiCB <137423039+SergiiCB@users.noreply.github.com>
2024-07-21 18:18:43 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,9 +16,12 @@ const translations = {
         emailPlaceholder: "Email",
         messagePlaceholder: "Mensaje",
         submitButton: "Enviar",
-        hello: "Hola, soy [Tu Nombre]",
-        developer: "Desarrollador de Software | Especialista en [Tu Especialidad]",
-        viewProjects: "Ver Mis Proyectos"
+        hello: "Hola, soy Sergi",
+        developer: "Data scientist | Especialista en Power BI, Tableau, Python, SQL, RStudio",
+        viewProjects: "Ver Mis Proyectos",
+        briefDescription: "Soy un estudiante de Ciencia de Datos en la UOC, apasionado por este campo y la programación en general. Me considero una persona responsable, comprometida y entusiasta, con una gran capacidad para trabajar en equipo. Actualmente, busco una oportunidad para realizar mis prácticas curriculares en una empresa innovadora que me permita seguir aprendiendo y desarrollando mis habilidades.",
+        footerText: "Portafolio | Sergi Cózar Badia",
+        languageButton: "Idioma"
     },
     en: {
         about: "About Me",
@@ -28,9 +31,12 @@ const translations = {
         emailPlaceholder: "Email",
         messagePlaceholder: "Message",
         submitButton: "Send",
-        hello: "Hello, I'm [Your Name]",
-        developer: "Software Developer | Specialist in [Your Specialty]",
-        viewProjects: "View My Projects"
+        hello: "Hello, I'm Sergi",
+        developer: "Data scientist | Specialist in Power BI, Tableau, Python, SQL, RStudio",
+        viewProjects: "View My Projects",
+        briefDescription: "I'm a data science student at the UOC with a passion for organizing and analyzing information. My approach to work is characterized by consistency, responsibility, and collaborative team environments. I am currently looking for an internship opportunity at a forward-thinking company to apply for and improve my skills. I am excited to learn and develop my future career in the field of data science.",
+        footerText: "Portfolio | Sergi Cózar Badia",
+        languageButton: "Language"
     },
     ca: {
         about: "Sobre Mi",
@@ -40,9 +46,12 @@ const translations = {
         emailPlaceholder: "Correu electrònic",
         messagePlaceholder: "Missatge",
         submitButton: "Enviar",
-        hello: "Hola, sóc [El Teu Nom]",
-        developer: "Desenvolupador de Programari | Especialista en [La Teva Especialitat]",
-        viewProjects: "Veure Els Meus Projectes"
+        hello: "Hola, sóc Sergi",
+        developer: "Data scientist | Especialista en Power BI, Tableau, Python, SQL, RStudio",
+        viewProjects: "Veure Els Meus Projectes",
+        briefDescription: "Soc un estudiant de Ciència de Dades de la UOC, apassionat per aquest camp i la programació en general. Em considero una persona responsable, compromesa i entusiasta, amb una gran capacitat per a treballar en equip. Actualment, busco una oportunitat per a realitzar les meves pràctiques curriculars en una empresa innovadora que em permeti continuar aprenent i desenvolupant les meves habilitats.",
+        footerText: "Portafoli | Sergi Cózar Badia",
+        languageButton: "Idioma"
     }
 };
 
@@ -50,18 +59,33 @@ document.querySelectorAll('.lang').forEach(link => {
     link.addEventListener('click', function(e) {
         e.preventDefault();
         const lang = this.getAttribute('data-lang');
+        
         document.getElementById('about').querySelector('h2').textContent = translations[lang].about;
+        document.getElementById('about').querySelector('p').textContent = translations[lang].briefDescription;
         document.getElementById('projects').querySelector('h2').textContent = translations[lang].projects;
         document.getElementById('contact').querySelector('h2').textContent = translations[lang].contact;
+        
         document.querySelector('input[name="name"]').placeholder = translations[lang].namePlaceholder;
         document.querySelector('input[name="email"]').placeholder = translations[lang].emailPlaceholder;
         document.querySelector('textarea[name="message"]').placeholder = translations[lang].messagePlaceholder;
         document.querySelector('button[type="submit"]').textContent = translations[lang].submitButton;
+        
         document.querySelector('.hero-content h1').textContent = translations[lang].hello;
         document.querySelector('.hero-content p').textContent = translations[lang].developer;
         document.querySelector('.hero .btn').textContent = translations[lang].viewProjects;
+        
+        document.querySelector('footer p').textContent = translations[lang].footerText;
+        
+        document.querySelectorAll('.nav-links li a').forEach((navLink, index) => {
+            if (index === 0) navLink.textContent = translations[lang].projects;
+            if (index === 1) navLink.textContent = translations[lang].about;
+            if (index === 2) navLink.textContent = translations[lang].contact;
+        });
+
+        document.getElementById('language-button').textContent = translations[lang].languageButton;
     });
 });
+
 
 // Boton Idiomas
 

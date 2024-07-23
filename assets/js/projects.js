02d01375a816114f3d6b6ac6e5564bef9eb78714
SergiCB <137423039+SergiiCB@users.noreply.github.com>
2024-07-23 20:41:59 +0200

    Add files via upload

---
+++ assets/js/projects.js
@@ -0,0 +1,268 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const translations = {
+    es: {
+        title: "Proyectos",
+        description: "Estos son algunos de mis proyectos",
+        toolsTitle: "Herramientas",
+        languageButton: "Idioma",
+        footerText: "&copy; Portafolio | Sergi Cózar Badia",
+        projects: [
+            {
+                category: "Tableau",
+                projects: [
+                    {
+                        title: "Incendios forestales en España",
+                        description: "Entre el 2001 y 2015",
+                        imgSrc: "images/projects/proyectoIFE.png",
+                        imgAlt: "Proyecto 1",
+                        url: "proyectoIFE.html"
+                    },
+                    {
+                        title: "Proyecto 2",
+                        description: "Descripción del proyecto 2",
+                        imgSrc: "images/projects/proyecto2.png",
+                        imgAlt: "Proyecto 2",
+                        url: "proyecto2.html"
+                    }
+                ]
+            },
+            {
+                category: "Python",
+                projects: [
+                    {
+                        title: "Proyecto 1",
+                        description: "Descripción del proyecto 1",
+                        imgSrc: "images/projects/proyecto1.png",
+                        imgAlt: "Proyecto 1",
+                        url: "proyecto1.html"
+                    },
+                    {
+                        title: "Proyecto 2",
+                        description: "Descripción del proyecto 2",
+                        imgSrc: "images/projects/proyecto2.png",
+                        imgAlt: "Proyecto 2",
+                        url: "proyecto2.html"
+                    }
+                ]
+            }
+        ]
+    },
+    en: {
+        title: "Projects",
+        description: "Here are some of my projects",
+        toolsTitle: "Tools",
+        languageButton: "Language",
+        footerText: "&copy; Portfolio | Sergi Cózar Badia",
+        projects: [
+            {
+                category: "Tableau",
+                projects: [
+                    {
+                        title: "Forest Fires in Spain",
+                        description: "Between 2001 and 2015",
+                        imgSrc: "images/projects/proyectoIFE.png",
+                        imgAlt: "Project 1",
+                        url: "proyectoIFE.html"
+                    },
+                    {
+                        title: "Project 2",
+                        description: "Project 2 description",
+                        imgSrc: "images/projects/proyecto2.png",
+                        imgAlt: "Project 2",
+                        url: "project2.html"
+                    }
+                ]
+            },
+            {
+                category: "Python",
+                projects: [
+                    {
+                        title: "Project 1",
+                        description: "Project 1 description",
+                        imgSrc: "images/projects/proyecto1.png",
+                        imgAlt: "Project 1",
+                        url: "project1.html"
+                    },
+                    {
+                        title: "Project 2",
+                        description: "Project 2 description",
+                        imgSrc: "images/projects/proyecto2.png",
+                        imgAlt: "Project 2",
+                        url: "project2.html"
+                    }
+                ]
+            }
+        ]
+    },
+    ca: {
+        title: "Projectes",
+        description: "Aquests són alguns dels meus projectes",
+        toolsTitle: "Eines",
+        languageButton: "Idioma",
+        footerText: "&copy; Portafoli | Sergi Cózar Badia",
+        projects: [
+            {
+                category: "Tableau",
+                projects: [
+                    {
+                        title: "Incendis forestals a Espanya",
+                        description: "Entre el 2001 i 2015",
+                        imgSrc: "images/projects/proyectoIFE.png",
+                        imgAlt: "Projecte 1",
+                        url: "proyectoIFE.html"
+                    },
+                    {
+                        title: "Projecte 2",
+                        description: "Descripció del projecte 2",
+                        imgSrc: "images/projects/proyecto2.png",
+                        imgAlt: "Projecte 2",
+                        url: "projecte2.html"
+                    }
+                ]
+            },
+            {
+                category: "Python",
+                projects: [
+                    {
+                        title: "Projecte 1",
+                        description: "Descripció del projecte 1",
+                        imgSrc: "images/projects/proyecto1.png",
+                        imgAlt: "Projecte 1",
+                        url: "projecte1.html"
+                    },
+                    {
+                        title: "Projecte 2",
+                        description: "Descripció del projecte 2",
+                        imgSrc: "images/projects/proyecto2.png",
+                        imgAlt: "Projecte 2",
+                        url: "projecte2.html"
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+function updateContent(language) {
+    const translation = translations[language];
+
+    // Actualizar títulos y descripciones generales
+    document.querySelector('.title-projects h1').innerText = translation.title;
+    document.querySelector('.title-projects p').innerText = translation.description;
+
+    // Actualizar botón de idioma
+    document.getElementById('language-button').innerText = translation.languageButton;
+
+    // Actualizar pie de página
+    document.querySelector('footer p').innerHTML = translation.footerText;
+
+    // Actualizar secciones de proyectos
+    const detailedProjects = document.querySelector('.detailed-projects');
+    detailedProjects.innerHTML = ''; // Limpiar contenido actual
+
+    translation.projects.forEach(category => {
+        // Crear y añadir la categoría del proyecto
+        const categoryTitle = document.createElement('h2');
+        categoryTitle.innerText = category.category;
+        detailedProjects.appendChild(categoryTitle);
+
+        const projectList = document.createElement('div');
+        projectList.classList.add('project-list');
+
+        category.projects.forEach(project => {
+            // Crear y añadir cada proyecto
+            const projectItem = document.createElement('div');
+            projectItem.classList.add('project-item');
+
+            const projectImg = document.createElement('img');
+            projectImg.classList.add('project-img');
+            projectImg.src = project.imgSrc;
+            projectImg.alt = project.imgAlt;
+            projectImg.setAttribute('data-url', project.url); // Añadir atributo data-url
+            projectItem.appendChild(projectImg);
+
+            const projectTitle = document.createElement('h3');
+            projectTitle.innerText = project.title;
+            projectItem.appendChild(projectTitle);
+
+            const projectDesc = document.createElement('p');
+            projectDesc.innerText = project.description;
+            projectItem.appendChild(projectDesc);
+
+            projectList.appendChild(projectItem);
+        });
+
+        detailedProjects.appendChild(projectList);
+    });
+
+    // Agregar eventos de click a las imágenes de los proyectos para redirección
+    const projectImages = document.querySelectorAll('.project-img');
+    projectImages.forEach(image => {
+        image.addEventListener('click', function() {
+            const url = this.getAttribute('data-url');
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    });
+}
+
+// Botón Idiomas
+
+document.addEventListener("DOMContentLoaded", function() {
+    const languageButton = document.getElementById('language-button');
+    const dropdown = document.querySelector('.dropdown');
+    const languageLinks = document.querySelectorAll('.lang');
+
+    function toggleDropdown() {
+        dropdown.classList.toggle('show');
+    }
+
+    languageButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        toggleDropdown();
+    });
+
+    languageLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            toggleDropdown();
+            const selectedLang = this.getAttribute('data-lang');
+            updateContent(selectedLang); // Actualizar contenido según el idioma seleccionado
+        });
+    });
+
+    document.addEventListener('click', function(e) {
+        if (!dropdown.contains(e.target) && !languageButton.contains(e.target)) {
+            dropdown.classList.remove('show');
+        }
+    });
+
+    // Inicializar el contenido en español por defecto
+    updateContent('es');
+});
+
+// Redirección web
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleccionar todas las imágenes de proyecto
+    const projectImages = document.querySelectorAll('.project-img');
+
+    // Agregar evento de click a cada imagen
+    projectImages.forEach(image => {
+        image.addEventListener('click', function() {
+            // Obtener la URL del atributo data-url
+            const url = this.getAttribute('data-url');
+            // Redirigir a la URL
+            window.location.href = url;
+        });
+    });
+});
---
+++ assets/js/scripts.js
@@ -0,0 +1,169 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const translations = {
+    es: {
+        about: "Sobre Mí",
+        projects: "Proyectos",
+        contact: "Contacto",
+        namePlaceholder: "Nombre",
+        emailPlaceholder: "Email",
+        messagePlaceholder: "Mensaje",
+        submitButton: "Enviar",
+        hello: "Hola, soy Sergi",
+        developer: "Data scientist | Especialista en Power BI, Tableau, Python, SQL, RStudio",
+        viewProjects: "Ver Mis Proyectos",
+        briefDescription: "Soy un estudiante de Ciencia de Datos en la UOC, apasionado por este campo y la programación en general. Me considero una persona responsable, comprometida y entusiasta, con una gran capacidad para trabajar en equipo. Actualmente, busco una oportunidad para realizar mis prácticas curriculares en una empresa innovadora que me permita seguir aprendiendo y desarrollando mis habilidades.",
+        footerText: "Portafolio | Sergi Cózar Badia",
+        languageButton: "Idioma"
+    },
+    en: {
+        about: "About Me",
+        projects: "Projects",
+        contact: "Contact",
+        namePlaceholder: "Name",
+        emailPlaceholder: "Email",
+        messagePlaceholder: "Message",
+        submitButton: "Send",
+        hello: "Hello, I'm Sergi",
+        developer: "Data scientist | Specialist in Power BI, Tableau, Python, SQL, RStudio",
+        viewProjects: "View My Projects",
+        briefDescription: "I'm a data science student at the UOC with a passion for organizing and analyzing information. My approach to work is characterized by consistency, responsibility, and collaborative team environments. I am currently looking for an internship opportunity at a forward-thinking company to apply for and improve my skills. I am excited to learn and develop my future career in the field of data science.",
+        footerText: "Portfolio | Sergi Cózar Badia",
+        languageButton: "Language"
+    },
+    ca: {
+        about: "Sobre Mi",
+        projects: "Projectes",
+        contact: "Contacte",
+        namePlaceholder: "Nom",
+        emailPlaceholder: "Correu electrònic",
+        messagePlaceholder: "Missatge",
+        submitButton: "Enviar",
+        hello: "Hola, sóc en Sergi.",
+        developer: "Data scientist | Especialista en Power BI, Tableau, Python, SQL, RStudio",
+        viewProjects: "Veure Els Meus Projectes",
+        briefDescription: "Soc un estudiant de Ciència de Dades de la UOC, apassionat per aquest camp i la programació en general. Em considero una persona responsable, compromesa i entusiasta, amb una gran capacitat per a treballar en equip. Actualment, busco una oportunitat per a realitzar les meves pràctiques curriculars en una empresa innovadora que em permeti continuar aprenent i desenvolupant les meves habilitats.",
+        footerText: "Portafoli | Sergi Cózar Badia",
+        languageButton: "Idioma"
+    }
+};
+
+document.querySelectorAll('.lang').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const lang = this.getAttribute('data-lang');
+        
+        document.getElementById('about').querySelector('h2').textContent = translations[lang].about;
+        document.getElementById('about').querySelector('p').textContent = translations[lang].briefDescription;
+        document.getElementById('projects').querySelector('h2').textContent = translations[lang].projects;
+        document.getElementById('contact').querySelector('h2').textContent = translations[lang].contact;
+        
+        document.querySelector('input[name="name"]').placeholder = translations[lang].namePlaceholder;
+        document.querySelector('input[name="email"]').placeholder = translations[lang].emailPlaceholder;
+        document.querySelector('textarea[name="message"]').placeholder = translations[lang].messagePlaceholder;
+        document.querySelector('button[type="submit"]').textContent = translations[lang].submitButton;
+        
+        document.querySelector('.hero-content h1').textContent = translations[lang].hello;
+        document.querySelector('.hero-content p').textContent = translations[lang].developer;
+        document.querySelector('.hero .btn').textContent = translations[lang].viewProjects;
+        
+        document.querySelector('footer p').textContent = translations[lang].footerText;
+        
+        document.querySelectorAll('.nav-links li a').forEach((navLink, index) => {
+            if (index === 0) navLink.textContent = translations[lang].projects;
+            if (index === 1) navLink.textContent = translations[lang].about;
+            if (index === 2) navLink.textContent = translations[lang].contact;
+        });
+
+        document.getElementById('language-button').textContent = translations[lang].languageButton;
+    });
+});
+
+
+// Boton Idiomas
+
+document.addEventListener("DOMContentLoaded", function() {
+    const languageButton = document.getElementById('language-button');
+    const dropdown = document.querySelector('.dropdown');
+    const languageLinks = document.querySelectorAll('.lang');
+
+    // Función para alternar la visibilidad del menú desplegable
+    function toggleDropdown() {
+        dropdown.classList.toggle('show');
+    }
+
+    // Evento para abrir/cerrar el menú al hacer clic en el botón "Idioma"
+    languageButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        toggleDropdown();
+    });
+
+    // Evento para cerrar el menú al seleccionar un idioma
+    languageLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            toggleDropdown();
+            // Aquí también puedes agregar la lógica para cambiar el idioma si aún no está implementada
+        });
+    });
+
+    // Evento para cerrar el menú si se hace clic fuera de él
+    document.addEventListener('click', function(e) {
+        if (!dropdown.contains(e.target) && !languageButton.contains(e.target)) {
+            dropdown.classList.remove('show');
+        }
+    });
+});
+
+// Carrusel de imagenes
+
+let slideIndex = 0;
+const slides = document.getElementsByClassName("mySlides");
+const progressBar = document.querySelector(".progress");
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    resetProgressBar();
+    setTimeout(showSlides, 5000); // Imagen cada 5 segundos
+}
+
+function resetProgressBar() {
+    progressBar.style.animation = "none";
+    void progressBar.offsetWidth; // Reinicia la animación
+    progressBar.style.animation = "progress 5s linear infinite";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    showSlides();
+});
+
+// Redirección web
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleccionar todas las imágenes de proyecto
+    const projectImages = document.querySelectorAll('.project-img');
+
+    // Agregar evento de click a cada imagen
+    projectImages.forEach(image => {
+        image.addEventListener('click', function() {
+            // Obtener la URL del atributo data-url
+            const url = this.getAttribute('data-url');
+            // Redirigir a la URL
+            window.location.href = url;
+        });
+    });
+});

b9fbb8df0ffa34e08ba54820fa010a1eb49f215e
SergiCB <137423039+SergiiCB@users.noreply.github.com>
2024-07-30 14:11:57 +0200

    Update project_1.js

--- assets/js/project_1.js
+++ assets/js/project_1.js
@@ -2,24 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const translations = {
         es: {
             mainTitle: "Incendios forestales en España entre 2001 y 2015",
-            presentationTitle: "Mi Presentación de Google Slides",
-            tableauTitle: "Gráfico de Tableau",
             slidesSrc: "https://docs.google.com/presentation/d/e/2PACX-1vQ-CS--c6B8Cuo7cObU3e23Vy09e0jgw51afAgYUV6Y3f9NS_os-jvo8f-xa0cZ8gQ6N0o04WfEIO2i/embed?start=false&loop=false&delayms=3000",
             languageButton: "Idioma",
             footerText: "© Portafolio | Sergi Cózar Badia"
         },
         en: {
             mainTitle: "Forest Fires in Spain between 2001 and 2015",
-            presentationTitle: "My Google Slides Presentation",
-            tableauTitle: "Tableau Chart",
             slidesSrc: "https://docs.google.com/presentation/d/e/2PACX-1vQropNqLmOOA0spLjFPMOJzDE5FSFTSTza0blueb2ABuy1YS-pR2Xny2Jo3YBy1ve8qQsRDX9YbOben/embed?start=false&loop=false&delayms=3000",
             languageButton: "Language",
             footerText: "© Portfolio | Sergi Cózar Badia"
         },
         ca: {
             mainTitle: "Incendis forestals a Espanya entre 2001 i 2015",
-            presentationTitle: "La Meva Presentació de Google Slides",
-            tableauTitle: "Gràfic de Tableau",
             slidesSrc: "https://docs.google.com/presentation/d/e/2PACX-1vT7jw5G5OgNZXPOB5azYFU5aESmPJOgvvcxaLG0X9CjwEXkT1yHgwWT5PM5mT8N7PZ7fAb-VIkdeUAU/embed?start=false&loop=false&delayms=3000",
             languageButton: "Idioma",
             footerText: "© Portafoli | Sergi Cózar Badia"
